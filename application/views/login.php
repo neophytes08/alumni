507a2c261cd348306d10f02b7357fc8d490ff11b
neophytes08 <allan.alzula@gmail.com>
2015-02-11 01:46:48 +0800

    login.php added assocation with s

--- application/views/login.php
+++ application/views/login.php
@@ -59,7 +59,7 @@
                     <span class="icon-bar"></span>
                 </button>
 
-                <a class="navbar-brand topnav title-site" href="#">Alumni Association</a>
+                <a class="navbar-brand topnav title-site" href="#">Alumni Associations</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
