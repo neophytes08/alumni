dd212b49947dc809f014467897765010a3a82eca
neophytes08 <allan.alzula@gmail.com>
2015-02-13 04:03:45 +0800

    another deploy changing base url

--- assets/scripts/loginFactory.js
+++ assets/scripts/loginFactory.js
@@ -2,7 +2,7 @@ login
 		.factory('getUrl',[
 			function factory(){
 				return {
-					url: '/alumni/index.php',
+					url: 'http://smcc-alumni.herokuapp.com/index.php',
 					header: {
 						headers: {
 							'Content-Type' : 'application/x-www-form-urlencoded'
